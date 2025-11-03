97c3e22d4c3afd35f238be4cbcb29a1fc535024e
Miled issa <miledaz1234@gmail.com>
2025-11-03 20:20:31 +0200

    improve the reset password

--- csis_231-login-registration-jwt/demo/src/main/java/com/example/demo/controllers/ResetController.java
+++ csis_231-login-registration-jwt/demo/src/main/java/com/example/demo/controllers/ResetController.java
@@ -10,7 +10,7 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 public class ResetController {
-    @FXML private TextField email;
+    String email;
     @FXML private TextField code;
     @FXML private PasswordField newPassword;
 
@@ -18,23 +18,23 @@ public class ResetController {
     public void initialize() {
         Platform.runLater(() -> {
             if (TempAuth.resetEmail != null && !TempAuth.resetEmail.isBlank()) {
-                email.setText(TempAuth.resetEmail);
+                email = (TempAuth.resetEmail);
             }
         });
     }
 
     public void onReset() {
         try {
-            String e = safe(email.getText());
+
             String c = safe(code.getText());
             String p = safe(newPassword.getText());
 
-            if (e.isEmpty() || c.isEmpty() || p.isEmpty()) {
-                AlertUtils.warn("Enter email, the code you received, and a new password.");
+            if ( c.isEmpty() || p.isEmpty()) {
+                AlertUtils.warn("Enter The code you received, and a new password.");
                 return;
             }
 
-            AuthApi.resetWithCode(e, c, p);
+            AuthApi.resetWithCode(email, c, p);
             AlertUtils.info("Password reset. Please login with your new password.");
             Launcher.go("login.fxml", "Login");
         } catch (Exception ex) {
