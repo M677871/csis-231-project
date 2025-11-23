0ea4de8ab514618d352f4aeb8c1aae3d6e62baf5
M677871 <miledaz1234@gmail.com>
2025-11-23 15:42:10 +0200

    fixing error 2

--- csis_231-login-registration-jwt/demo/src/main/java/com/example/demo/model/ResetPasswordRequest.java
+++ csis_231-login-registration-jwt/demo/src/main/java/com/example/demo/model/ResetPasswordRequest.java
@@ -5,12 +5,12 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 /**
  * Request body used by the client to complete the password-reset flow.
  *
- * @param emailOrUsername the account identifier (e-mail or username) used to request the reset
- * @param token           the password-reset code (OTP) received by the user
- * @param newPassword     the new password that should be stored for this account
+ * @param email       e-mail for which the reset OTP was requested
+ * @param code        the password-reset code (OTP) received by the user
+ * @param newPassword the new password that should be stored for this account
  */
 public record ResetPasswordRequest(
-        String emailOrUsername,
-        @JsonProperty("code") String token,
+        @JsonProperty("email") String email,
+        @JsonProperty("code") String code,
         String newPassword
 ) {}
